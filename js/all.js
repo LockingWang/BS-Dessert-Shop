b22e81f31da05cd45f321d5b273c7f76d85cb744
“Harry” <boom0525300@gmail.com>
2023-03-16 23:12:48 +0800

    items pages finish

--- js/all.js
+++ js/all.js
@@ -3,10 +3,22 @@ $(document).ready(function () {
     // 圖片切換效果(內容圖片消失、顯示背景圖片)
     $('.link-hover').on('mouseover', function () {
         $(this).parent().siblings('.bg-img').children().animate({ opacity: 0 }, 100);
-        // $(this).parent('.card').children('.img-fade').animate({ opacity: 0 }, 100);
     });
     $('.link-hover').on('mouseout', function () {
         $(this).parent().siblings('.bg-img').children().animate({ opacity: 1 }, 100);
     });
 
+    // list hover 顯示隱藏文字
+    $('.list-hover').on('mouseover', function () {
+        $(this).children('span').removeClass('list-hide');
+        $(this).children('span').addClass('list-show');
+
+        $(this).addClass('transform');
+    });
+    $('.list-hover').on('mouseout', function () {
+        $(this).children('span').removeClass('list-show');
+        $(this).children('span').addClass('list-hide');
+
+        $(this).removeClass('transform');
+    });
 });
